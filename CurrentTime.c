a38ab8acc1ff3d6e98c00396f1b03e0c9c89393d
Lee ChangRyoel <dlckdfuf141@naver.com>
2020-07-07 14:34:14 +0900

    Create CurrentTime.c

---
+++ CurrentTime.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<time.h>
+#include<Windows.h>
+#pragma warning(disable: 4996)
+int main()
+{
+	time_t timer;
+	do {
+		time(&timer);
+		char* tm = asctime(localtime(&timer));
+		system("cls");
+		printf("현재시간: %s\n",tm);
+	} while (1);
+	return 0;
+}
